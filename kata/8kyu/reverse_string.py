e6a1e0d8dc765edc18c6d60af4a6615b456dfa4c
stevenedixon <stevenedixon@outlook.com>
2020-03-10 00:44:03 +0000

    Additional

--- kata/8kyu/reverse_string.py
+++ kata/8kyu/reverse_string.py
@@ -1,14 +1,31 @@
 # reverse a string
+import string
 
 
-def reverseWords(input):
-    inputWords = input.split(" ")
-    inputWords = inputWords[-1::-1]
+def reverseWords(input_string):
+    input_words = input_string.split(" ")
+    input_words = input_words[-1::-1]
 
-    output = ' '.join(inputWords)
+    output = ' '.join(input_words)
+
+    return output
+
+
+def removePunctuationAndReverseWords(input_string):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+
+    for x in input_string.lower():
+        if x in punctuations:
+            input_string = input_string.replace(x, "")
+
+    input_words = input_string.split(" ")
+    input_words = input_words[-1::-1]
+    output = ' '.join(input_words)
 
     return output
 
 
 print(reverseWords("test output"))  # output test
-print(reverseWords("hi, my name is steve!"))  # output test
+print(reverseWords("hi, my name is steve!"))  # steve! is name my hi,
+print(removePunctuationAndReverseWords("test one!"))  # one test
+print(removePunctuationAndReverseWords("this!! is!! !a! test?!@"))  # test a is this
